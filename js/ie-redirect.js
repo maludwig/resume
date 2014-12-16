5310e740117421bfaf1e5bb8cccaeeaa2371b1f3
Mitchell Ludwig <mitchell.ludwig@gmail.com>
2014-12-16 15:55:06 -0700

    Cleaned up old files

--- js/ie-redirect.js
+++
@@ -1,8 +0,0 @@
-/* global navigator, $, window */
-function isIE() {
-    var myNav = navigator.userAgent.toLowerCase();
-    return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
-}
-if (isIE() && isIE() < 10) {
-    window.location.href += "ie/";
-}
