91ae67aae280171b82aeae9712ce0daffbcefd58
Mitchell Ludwig <mitchell.ludwig@gmail.com>
2014-12-15 19:04:46 -0700

    Almost final revision

---
+++ workexperience.php
@@ -0,0 +1,68 @@
+<div class="job">
+    <h3>Edgemont Computer Geek (Personal Company)</h3>
+    <h4>November 2006 – Present Day</h4>
+    <h5>Web Administrator / Web Developer</h5>
+    <ul>
+        <li>Designed, installed, and maintained multiple Windows Server and cloud-based virtualized Linux servers (Amazon EC2) for high reliability</li>
+        <li>Implemented a great many web sites with LAMP (Linux, Apache, MySQL, PHP).</li>
+        <li>Frequently required looking over server logs, monitoring and optimizing system performance, and managing user accounts and permissions</li>
+        <li>Provided onsite and offsite professional technical support</li>
+        <li>Troubleshot SOHO networks, solving issues with a variety of systems including resource access, virus removal, printer, e-mail, network, and security setup</li>
+    </ul>
+</div>
+
+<div class="job">
+    <h3>Mark’s (Through FGL Sports)</h3>
+    <h4>February 2014 – September 2014</h4>
+    <h5>In-Store Support Technician, Level 3</h5>
+    <ul>
+        <li>Installed, Maintained, Troubleshot a variety of equipment in individual stores, including cash registers, advertising equipment, network equipment, and desktop/laptop computers</li>
+        <li>Implemented web app to automate common tasks, and make them available to the TSC (Level 1 support desk) so that they could increase their first-call resolution rate, and help the stores get back up and running more efficiently</li>
+        <li>Maintained, Troubleshot server equipment that supported the stores directly, including print servers, update servers, banking servers, and file servers</li>
+        <li>Provided onsite and offsite professional technical support</li>
+    </ul>
+</div>
+    
+<div class="job">
+    <h3>Talisman Energy – Through TEKSystems </h3>
+    <h4>June 2011 – September 2012</h4>
+    <h5>Desktop Support Technician, Level IV</h5>
+    <ul>
+        <li>Designed, implemented web based asset tracking system, which integrated automated queries to find lost equipment </li>
+        <li>Designed, implemented a web based network monitoring suite </li>
+        <li>Designed, implemented an online store to track equipment </li>
+        <li>Was the final escalation point in my team of 40 people, if nobody else could figure it out, it came to me </li>
+        <li>Performed networking tasks with a system of over 130 switches with 2000 users </li>
+    </ul>
+</div>
+
+<div class="job">
+    <h3>IBM, Superstore, Futureshop, Best Buy, Walmart, The Bay, RBC Royal Bank, and TD Bank – Through Kelly Services </h3>
+    <h4>February 2010 – September 2010 </h4>
+    <h5>POS Technician </h5>
+</div>
+<div class="job">
+    <h3>Shell Oil – Through TEKSystems</h3>
+    <h4>March 2009 - April 2009 </h4>
+    <h5>Desktop Support Technician, Level III </h5>
+</div>
+<div class="job">
+    <h3>General Dynamics Canada – Through Sapphire Technologies</h3>
+    <h4>December 2008 – February 2009 </h4>
+    <h5>Desktop Support Technician, Level III </h5>
+</div>
+<div class="job">
+    <h3>Calgary Board of Education</h3>
+    <h4>September 2006 – January 2008 </h4>
+    <h5>Desktop Technician, Level II </h5>
+</div>
+<div class="job">
+    <h3>SMART Technologies – Through Robert Half Technologies</h3>
+    <h4>August 2008 – October 2008</h4>
+    <h5>Help Desk, Level II</h5>
+</div>
+<div class="job">
+    <h3>Calgary Stampede</h3>
+    <h4>May 2006 – August 2006, May 2007 – August 2007</h4>
+    <h5>POS Technician</h5>
+</div>
