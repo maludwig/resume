f45eb0d7360b022bb52efe26740de05f3212ad57
Mitchell Ludwig <mitchell.ludwig@gmail.com>
2015-09-16 03:17:57 -0600

    Added parallax

--- ie/index.php
+++ ie/index.php
@@ -1,52 +1,44 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>IE Sympathy Page</title>
 	<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </head>
 
 <body>
 	<div class="container">
-        <div class="page-header">
-            <h1>Internet Explorer detected</h1>
-        </div>
-        <p>
-            We have detected that you're using an older version of Internet Explorer to attempt to view this resume. 
-            Internet Explorer is actually horrible. It's so horrible, <a href="https://www.modern.ie/en-us/ie6countdown">even Microsoft wants you to stop using it.</a>
-            I didn't even make a page to detect older versions of other browsers, because the update mechanisms in other browsers are actually decent, and the odds of you using, like, Chrome v6 are ridiculously improbable.
-        </p>
-        <p>
-            It's literally just IE that's horrible enough to get its own failure page.
-        </p>
-        <p>
-            This site is supposed to be a tech demo for showing that I know how to code in modern languages, like CSS3 and HTML5. If you want to see all that fancy stuff, you'll need to access the site with a modern browser.
-            But I've made a Whiteboard web app that's designed to be compatible with earlier versions of IE, which you can check out if you want to make sure I can code for older browsers, as well as new.
-            Once you're done checking it out, unless this is a test of the compatibility of my code, I strongly recommend seeking the aid of a mental health professional. 
-            If you're using IE willingly, it's a clear indicator of outstanding mental health issues.
-        </p>
-        <p><a class="btn btn-lg btn-success" href="http://edgemontgeek.com/whiteboard" role="button">Whiteboard App</a></p>
-        <p>
-            You may also access my resume in PDF or Microsoft Word formats. If you're having trouble opening these files you need to get Adobe Rea- actually...if you can't open either a PDF or a DOC...then I don't want to work for your company. 
-            Seriously, what is this, the stone age?
-            I wish you the best of luck finding another candidate.
-        </p>
-        <p>
-            <a class="btn btn-lg btn-danger" href="../files/MitchellLudwig.pdf" role="button">
-                <img src="../img/pdf.png" />
-                PDF Version
-            </a>
-            <a class="btn btn-lg btn-primary" href="../files/MitchellLudwig.doc" role="button">
-                <img src="../img/winword.png" />
-                MS Word Version
-            </a>
-        </p>
+		<div class="page-header">
+			<h1>Internet Explorer detected</h1>
+		</div>
+		<p>
+			We have detected that you're using an older version of Internet Explorer to attempt to view this resume. Internet Explorer is actually horrible. It's so horrible, <a href="https://www.modern.ie/en-us/ie6countdown">even Microsoft wants you to stop using it.</a> I didn't even make a page to detect older versions of other browsers, because the update mechanisms in other browsers are actually decent, and the odds of you using, like, Chrome v6 are ridiculously improbable.
+		</p>
+		<p>
+			It's literally just IE that's horrible enough to get its own failure page.
+		</p>
+		<p>
+			This site is supposed to be a tech demo for showing that I know how to code in modern languages, like CSS3 and HTML5. If you want to see all that fancy stuff, you'll need to access the site with a modern browser. But I've made a Whiteboard web app that's designed to be compatible with earlier versions of IE, which you can check out if you want to make sure I can code for older browsers, as well as new. Once you're done checking it out, unless this is a test of the compatibility of my code, I strongly recommend seeking the aid of a mental health professional. If you're using IE willingly, it's a clear indicator of outstanding mental health issues.
+		</p>
+		<p><a class="btn btn-lg btn-success" href="http://edgemontgeek.com/whiteboard" role="button">Whiteboard App</a></p>
+		<p>
+			You may also access my resume in PDF or Microsoft Word formats. If you're having trouble opening these files you need to get Adobe Rea- actually...if you can't open either a PDF or a DOC...then I don't want to work for your company. Seriously, what is this, the stone age? I wish you the best of luck finding another candidate.
+		</p>
+		<p>
+			<a class="btn btn-lg btn-danger" href="../files/MitchellLudwig.pdf" role="button">
+				<img src="../img/pdf.png" /> PDF Version
+			</a>
+			<a class="btn btn-lg btn-primary" href="../files/MitchellLudwig.doc" role="button">
+				<img src="../img/winword.png" /> MS Word Version
+			</a>
+		</p>
 	</div>
 	<div id="footer">
 	</div>
 </body>
-</html>
 
+</html>
--- index.php
+++ index.php
@@ -1,125 +1,145 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Resume - Mitchell Ludwig</title>
-    <!-- Hi! So you've decided to take a peek at the code. Awesome! I've kept everything purposefully unminified, so that you can look at the code, and easily understand the structure. -->
-    <!-- If you're familiar with GitHub, you can take a closer look at the code by checking out the repo: https://github.com/maludwig/resume -->
-    <script src="js/ie-redirect.js"></script><!-- redirect IE9 and under to the sympathy page -->
-    <!-- I'm using Less as my CSS compiler, because it has a rich feature set of awesome shorthand that really helps with speeding up development. On a normal web page, these would be precompiled server-side. I've left the less link here for your viewing pleasure. -->
-    <!-- <link rel="stylesheet/less" type="text/css" href="less/style.less" />
-    <script src="js/less-2.0.0.min.js"></script> -->
-    <link rel="stylesheet" type="text/css" href="css/style.css" />
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700,300' rel='stylesheet' type='text/css'><!-- A pretty font -->
-    <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'><!-- Exotic header font -->
-    <script src="js/jquery-1.11.1.min.js"></script><!-- If you don't know what jQuery is, why are you looking at my code? -->
-    <script src="js/jquery-center.1.2.js"></script><!-- Homebrew jQuery for centering objects in the DOM -->
 
-    <script>
-        $(window).load(function(){
-            $(".tile div img").center(); //Center the education images. Must be done after page load, unless image widths/heights are prespecified.
-        });
-    </script>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Resume - Mitchell Ludwig</title>
+	<!-- Hi! So you've decided to take a peek at the code. Awesome! I've kept everything purposefully unminified, so that you can look at the code, and easily understand the structure. -->
+	<!-- If you're familiar with GitHub, you can take a closer look at the code by checking out the repo: https://github.com/maludwig/resume -->
+	<script src="js/ie-redirect.js"></script>
+	<!-- redirect IE9 and under to the sympathy page -->
+	<!-- I'm using Less as my CSS compiler, because it has a rich feature set of awesome shorthand that really helps with speeding up development. On a normal web page, these would be precompiled server-side. I've left the less link here for your viewing pleasure. -->
+	<!--
+	<link rel="stylesheet/less" type="text/css" href="less/style.less" />
+	<script src="js/less-2.0.0.min.js"></script>
+	-->
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700,300' rel='stylesheet' type='text/css'>
+	<!-- A pretty font -->
+	<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+	<!-- Exotic header font -->
+	<script src="js/jquery-1.11.1.min.js"></script>
+	<!-- If you don't know what jQuery is, why are you looking at my code? -->
+	<script src="js/jquery-center.1.2.js"></script>
+	<!-- Homebrew jQuery for centering objects in the DOM -->
+	<script src="js/parallax.min.js"></script>
+	<!-- Homebrew jQuery for centering objects in the DOM -->
+
+	<script>
+		$(window).load(function () {
+			$(".tile div img").center(); //Center the education images. Must be done after page load, unless image widths/heights are prespecified.
+		});
+		$(window).resize(function () {
+			$(".tile div img").center(); //Center the education images. Must be done after page load, unless image widths/heights are prespecified.
+		});
+		$(function () {
+			$(".parallax-window").parallax({
+				imageSrc: "img/bg2.jpg",
+				naturalWidth: 1280,
+				naturalHeight: 2500,
+			});
+		});
+	</script>
 </head>
 
 <body>
-    <div id="container">
-        <header>
-            <h1>Mitchell Ludwig</h1>
-            <h3><a href="tel:4034793269">403-479-3269</a> &nbsp;&nbsp;&nbsp; <a href="mailto:mitchell.ludwig@gmail.com">mitchell.ludwig@gmail.com</a></h3>
-        </header>
-        <div id="content">
-            <section>
-                <h2>Other Versions</h2>
-                <div class="scontent">
-                    <div class="file"><a class="pdf" href="files/MitchellLudwig.pdf">PDF Version</a></div>
-                    <div class="file"><a class="doc" href="files/MitchellLudwig.doc">DOC Version</a></div>
-                </div>
-            </section>
-            <section>
-                <h2>Tech Demo</h2>
-                <div class="scontent">
-                    The <a href="http://edgemontgeek.com/whiteboard/">Whiteboard App</a> is a web app I designed and implemented to showcase my web development skills. It allows for people to share a whiteboard in the cloud, and visually communicate ideas over any distance. Built on LAMP (Linux [CentOS], Apache, MySQL, PHP) in the Amazon EC2 cloud.
-                </div>
-            </section>
-            <section>
-                <h2>Other Sample Work</h2>
-                <div class="scontent">
-                    <p><a href="http://cross-consultants.com/">Cross Consultants</a> is a security firm. Cloud hosted, built on PHP. While an extremely simple site, it does feature a quiz in the Learn section, that generates a certificate.</p>
-                    <p><a href="http://thecalgarycollection.ca/">The Calgary Collection</a> is a folk music collection, curated by the amazing Gillian M<sup>c</sup>Kercher. Cloud hosted (Amazon EC2), built on LAMP, powered by Wordpress. Scalable architecture was leveraged to handle large volumes during peak times. </p>
-                    <p><a href="https://github.com/maludwig">Latest GitHub account</a> | <a href="https://github.com/turiyag">First GitHub account</a></p>
-                </div>
-            </section>
-            <section>
-                <h2>Education</h2>
-                <div class="scontent">
-                    <div class="tile">
-                        <div><img src="img/edu-UofC.png" /></div>
-                        <p>University of Calgary (2008-2013) Computer Science, Concentration in Information Security</p>
-                    </div>
-                    <div class="tile">
-                        <div><img src="img/edu-CompTIA.png" /></div>
-                        <p>CompTIA A+, Network+, and Security+ Certified</p>
-                    </div>
-                    <div class="tile">
-                        <div><img src="img/edu-Microsoft.png" /></div>
-                        <p>Microsoft Certified Professional (MCP), Microsoft Certified Systems Administrator (MCSA)</p>
-                    </div>
-                    <div class="tile">
-                        <div><img src="img/edu-W3Schools-sofar.png" /></div>
-                        <p>CSS3 and Javascript Certified by W3Schools</p>
-                    </div>
-                </div>
-            </section>
-            <section>
-                <h2>Web Development Experience</h2>
-                <div class="scontent">
-                    <ul>
-                        <li>Extensive experience with dealing directly with clients to provide exactly what the client wants</li>
-                        <li>Proficient with modern web technologies, including:</li>
-                        <ul>
-                            <li>HTML5</li>
-                            <li>CSS3</li>
-                            <li>JavaScript</li>
-                            <li>XML</li>
-                            <li>AJAX</li>
-                            <li>jQuery</li>
-                            <li>Bootstrap</li>
-                            <li>Context Management System (Wordpress)</li>
-                        </ul>
-                        <li>Proficient with many back-end technologies, including </li>
-                        <ul>
-                            <li>LAMP/WAMP: (Linux/Windows, PHP, MySQL, Apache)</li>
-                            <li>ISS: ASP.NET (C#), Java</li>
-                            <li>SSH/SSL, git, FTP, DNS</li>
-                            <li>Virtualization (Amazon Cloud, VMWare)</li>
-                            <li>Scripting (bash, VBScript, JScript)</li>
-                            <li>git: Version control software</li>
-                        </ul>
-                    </ul>
-                </div>
-            </section>
-            <section>
-                <h2>Web Administration Experience</h2>
-                <div class="scontent">
-                    <ul>
-                        <li>Extensive experience as a technician, working at every level, from basic desktop computers to maintaining massive scale server architecture, including virtual environments</li>
-                        <li>Extensive experience with Windows Server products</li>
-                        <li>Primary experience is with cloud-based (Amazon EC2) virtualized Linux (CentOS) servers</li>
-                    </ul>
-                </div>
-            </section>
-            <section>
-                <h2>Work History</h2>
-                <div class="scontent">
-                    <?php include("./workexperience.php"); ?>
-                </div>
-            </section>
-            &copy; 2014 - Mitchell Ludwig
-        </div>
-    </div>
+	<div id="container" class="parallax-window">
+		<header>
+			<h1>Mitchell Ludwig</h1>
+			<h3><a href="tel:4034793269">403-479-3269</a> &nbsp;&nbsp;&nbsp; <a href="mailto:mitchell.ludwig@gmail.com">mitchell.ludwig@gmail.com</a></h3>
+		</header>
+		<div id="content">
+			<section>
+				<h2>Other Versions</h2>
+				<div class="scontent">
+					<div class="file"><a class="pdf" href="files/MitchellLudwig.pdf">PDF Version</a></div>
+					<div class="file"><a class="doc" href="files/MitchellLudwig.doc">DOC Version</a></div>
+				</div>
+			</section>
+			<section>
+				<h2>Tech Demo</h2>
+				<div class="scontent">
+					The <a href="http://edgemontgeek.com/whiteboard/">Whiteboard App</a> is a web app I designed and implemented to showcase my web development skills. It allows for people to share a whiteboard in the cloud, and visually communicate ideas over any distance. Built on LAMP (Linux [CentOS], Apache, MySQL, PHP) in the Amazon EC2 cloud.
+				</div>
+			</section>
+			<section>
+				<h2>Other Sample Work</h2>
+				<div class="scontent">
+					<p><a href="http://cross-consultants.com/">Cross Consultants</a> is a security firm. Cloud hosted, built on PHP. While an extremely simple site, it does feature a quiz in the Learn section, that generates a certificate.</p>
+					<p><a href="http://thecalgarycollection.ca/">The Calgary Collection</a> is a folk music collection, curated by the amazing Gillian M<sup>c</sup>Kercher. Cloud hosted (Amazon EC2), built on LAMP, powered by Wordpress. Scalable architecture was leveraged to handle large volumes during peak times. </p>
+					<p><a href="https://github.com/maludwig">Latest GitHub account</a> | <a href="https://github.com/turiyag">First GitHub account</a></p>
+				</div>
+			</section>
+			<section>
+				<h2>Education</h2>
+				<div class="scontent">
+					<div class="tile">
+						<div><img src="img/edu-UofC.png" /></div>
+						<p>University of Calgary (2008-2013) Computer Science, Concentration in Information Security</p>
+					</div>
+					<div class="tile">
+						<div><img src="img/edu-CompTIA.png" /></div>
+						<p>CompTIA A+, Network+, and Security+ Certified</p>
+					</div>
+					<div class="tile">
+						<div><img src="img/edu-Microsoft.png" /></div>
+						<p>Microsoft Certified Professional (MCP), Microsoft Certified Systems Administrator (MCSA)</p>
+					</div>
+					<div class="tile">
+						<div><img src="img/edu-W3Schools-sofar.png" /></div>
+						<p>CSS3 and Javascript Certified by W3Schools</p>
+					</div>
+				</div>
+			</section>
+			<section>
+				<h2>Web Development Experience</h2>
+				<div class="scontent">
+					<ul>
+						<li>Extensive experience with dealing directly with clients to provide exactly what the client wants</li>
+						<li>Proficient with modern web technologies, including:</li>
+						<ul>
+							<li>HTML5</li>
+							<li>CSS3</li>
+							<li>JavaScript</li>
+							<li>XML</li>
+							<li>AJAX</li>
+							<li>jQuery</li>
+							<li>Bootstrap</li>
+							<li>Context Management System (Wordpress)</li>
+						</ul>
+						<li>Proficient with many back-end technologies, including </li>
+						<ul>
+							<li>LAMP/WAMP: (Linux/Windows, PHP, MySQL, Apache)</li>
+							<li>ISS: ASP.NET (C#), Java</li>
+							<li>SSH/SSL, git, FTP, DNS</li>
+							<li>Virtualization (Amazon Cloud, VMWare)</li>
+							<li>Scripting (bash, VBScript, JScript)</li>
+							<li>git: Version control software</li>
+						</ul>
+					</ul>
+				</div>
+			</section>
+			<section>
+				<h2>Web Administration Experience</h2>
+				<div class="scontent">
+					<ul>
+						<li>Extensive experience as a technician, working at every level, from basic desktop computers to maintaining massive scale server architecture, including virtual environments</li>
+						<li>Extensive experience with Windows Server products</li>
+						<li>Primary experience is with cloud-based (Amazon EC2) virtualized Linux (CentOS) servers</li>
+					</ul>
+				</div>
+			</section>
+			<section>
+				<h2>Work History</h2>
+				<div class="scontent">
+					<?php include("./workexperience.php"); ?>
+				</div>
+			</section>
+			&copy; 2015 - Mitchell Ludwig
+		</div>
+	</div>
 </body>
-</html>
 
+</html>
